7dbf9c7f69717e4e86270037acd52c88342006c9
nabilgha-creator <nabil.gha@gmail.com>
2026-09-15 18:20:45 +0200

    BACKEND - VÉRIFICATION CHUNK.TXT

--- backend/chemin.py
+++ backend/chemin.py
@@ -4,3 +4,8 @@ RACINE = Path(__file__).resolve().parent.parent
 DOCS = RACINE / "assets" / "Documentation_Electrodomus"
 if not DOCS.exists():
     raise FileNotFoundError
+
+ASSETS = RACINE / "assets"
+
+if not ASSETS.exists() :
+    raise FileNotFoundError
--- backend/data.py
+++ backend/data.py
@@ -1,6 +1,6 @@
 
 from pathlib import Path
-from chemin import DOCS
+from chemin import DOCS , ASSETS
 from langchain_docling import DoclingLoader
 
 from chunker import chunker
@@ -11,18 +11,22 @@ files = [c for c in DOCS.rglob("*") if c.is_file() and not c.name.startswith("~$
 
 def charger_doc (fichier : list[str] | str) :
     liste_doc = []
-
-    for x in fichier :
+    counter = 0 
+    for x in fichier : 
         try:
             doc = DoclingLoader(file_path= str(x) , chunker=chunker)
             file = doc.load()
             liste_doc.extend(file)
+            counter += 1
             
         except Exception as e :
             print(f'exception {e} sur le fichier {x}')
-    print(len(files))
-    print(len(liste_doc))
     return liste_doc
          
-if __name__ == "main" :
+if __name__ == "__main__" :
     document = charger_doc(files)
+    
+    with open(f"{ASSETS}/chunks.txt", "w", encoding="utf-8") as f:
+        for x in document :
+            f.write(f"{x.page_content} ================================================\n")
+        
